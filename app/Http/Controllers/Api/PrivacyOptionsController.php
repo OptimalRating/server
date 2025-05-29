efcbbb772096fc3a4e3c77e84276ad37f741faba
OptimalRating <optimalrating@gmail.com>
2025-05-29 13:13:14 +0000

    updated_4

--- app/Http/Controllers/Api/PrivacyOptionsController.php
+++ app/Http/Controllers/Api/PrivacyOptionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\PrivacyOptions;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PrivacyOptionsController extends Controller
 {
