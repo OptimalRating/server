94efa848837470a5c2a9a31ba9a6ed8aeec2ee4f
OptimalRating <optimalrating@gmail.com>
2025-10-23 10:02:07 +0000

    added country model in keyword controller

--- app/Http/Controllers/Api/KeywordsController.php
+++ app/Http/Controllers/Api/KeywordsController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
+use App\Country;
 
 class KeywordsController extends Controller
 {
